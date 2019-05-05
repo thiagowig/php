96d0613f4dbdb68103f7308eb5fe84a660900440
Thiago Santos Fonseca <dev.thiago@gmail.com>
2019-05-05 14:00:49 -0300

    Creating some tests for PHP

---
+++ docker/files/index.php
@@ -0,0 +1,33 @@
+<? 
+    # Single line printing
+    echo "Thiago Santos Fonseca\n"; 
+    
+    print "Cruzeiro Esporte Clube\n";
+    
+    /* 
+        Multiple
+        lines
+        printing
+    */
+    print "    Thiago
+    Santos
+    Fonseca\n";
+
+    # Variable definition
+    $myName = "Thiago";
+    $mySalary = 30000;
+    print("My name is $myName and my salary is $mySalary \n");
+
+    # Statements
+    if ($myName == "Thiago") {
+        print("SAY MY NAME $myName \n");
+    } else {
+        print("You didnt say my name \n");
+    }
+
+    if ($mySalary == 30001) {
+        print("You didnt say my salary \n");
+    } else {
+        print("SAY MY SALARY $mySalary \n");
+    }
+?>
